72125317fa5eadc4a7a04cfc16ae48cb19d552ad
Gukil Kim <kki0287@naver.com>
2025-05-07 10:25:44 +0900

    초기 설정

---
+++ src/main/java/com/pro/StagelinkApplication.java
@@ -0,0 +1,13 @@
+package com.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagelinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StagelinkApplication.class, args);
+    }
+
+}
