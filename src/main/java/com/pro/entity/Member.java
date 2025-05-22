ae10e2a4d3cc70deca87b710d9db9677ff6575aa
YUNA <dbskwh11@naver.com>
2025-05-22 18:19:30 +0900

    refactor: 멤버 엔티티 수정

--- src/main/java/com/pro/entity/Member.java
+++ src/main/java/com/pro/entity/Member.java
@@ -1,9 +1,13 @@
 package com.pro.entity;
 
+
 import jakarta.persistence.*;
 import lombok.*;
 
+
 import java.time.LocalDate;
+
+
 
 
 @Entity
@@ -16,10 +20,12 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Member {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "MEMBER_NO", nullable = false) // 회원번호 (PK)
+    @Column(name = "MEMBER", nullable = false) // 회원번호 (PK)
     private Long id;
+
 
     @Column(name = "ID", length = 50, nullable = false, unique = true)
     private String userId;  // 로그인용 ID
@@ -28,8 +34,10 @@ public class Member {
     @Column(name = "NAME",length = 10)
     private String name;
 
+
     @Column(name = "BIRTHDAY")
     private LocalDate birthday;
+
 
     @Enumerated(EnumType.STRING)
     @Column(name = "GENDER")
@@ -37,8 +45,10 @@ public class Member {
     @Column(name = "NICKNAME", length = 20, nullable = false)
     private String nickname;
 
+
     @Column(name = "USER_EMAIL", length = 50, nullable = false, unique = true)
     private String userEmail;
+
 
     @Enumerated(EnumType.STRING)
     @Column(name = "MEMBER_STATE", nullable = false)
