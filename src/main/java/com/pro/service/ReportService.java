6dbaaba14f2e015c0e79e37ab29fef40d6f45cbd
Lee <leemi8443@naver.com>
2025-05-22 10:11:30 +0900

    컬럼명 수정 + devMode 사용.

--- src/main/java/com/pro/service/ReportService.java
+++ src/main/java/com/pro/service/ReportService.java
@@ -1,7 +1,9 @@
 package com.pro.service;
 
 import com.pro.dto.ReportRequestDto;
+import com.pro.entity.Post;
 import com.pro.entity.Report;
+import com.pro.repository.PostRepository;
 import com.pro.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,10 @@ import java.time.LocalDateTime;
 public class ReportService {
 
     private final ReportRepository reportRepository;
+    private final PostRepository postRepository;
 
     public void reportPost(ReportRequestDto dto) {
+        // 신고 저장
         Report report = new Report();
         report.setPostNo(dto.getPostNo());                    // 신고 대상 게시글 번호
         report.setSuspectId(dto.getSuspectId());    // 피신고자
@@ -23,5 +27,11 @@ public class ReportService {
         report.setReportDate(LocalDateTime.now());      // 신고 날짜
 
         reportRepository.save(report);
+
+        // 신고 카운트 증가
+        Post post = postRepository.findById(dto.getPostNo().intValue())
+                .orElseThrow(() -> new RuntimeException("해당 게시글을 찾을 수 없습니다."));
+        post.setPostReportCount(post.getPostReportCount() + 1); // 신고 수 1 증가
+        postRepository.save(post);
     }
 }
